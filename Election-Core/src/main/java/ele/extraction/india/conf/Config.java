fafaa453982c00a32e6056b5a47b820164721598
root <root@dhanu-HP-ProBook-4430s>
2015-04-02 14:20:47 +0530

    config change.

--- Election-Core/src/main/java/ele/extraction/india/conf/Config.java
+++ Election-Core/src/main/java/ele/extraction/india/conf/Config.java
@@ -49,6 +49,22 @@ public class Config {
 	}
 
 	public static String getSpreadSheet() {
+		switch (Integer.parseInt(prop.getProperty("year"))) {
+		case 1991:
+			return  "1eMyNnPnE0OQaP5tUeb4y7GMcdfxJUxlyxqVaT3SbQOk";
+		case 1989:
+			return  "1jM1zIyL-bh39k6yD3CVgSwKYVlyn4V08mISXXMgK3es";
+		case 1984:
+			return  "10b7WUKjpIEijwdXVowpoGJ6KqRkF3uTZ7dU1DOoDxhk";
+		case 1980:
+			return  "1JvXRiwASjNYdFbl3GT9ntpKp-70ocLaZo8bXEjl_kYM";
+		case 1977:
+			return  "1BMxkadSI_KeQvGQnSMjrFZdoqrfoapZtzkmdYywBo8I";
+		case 1971:
+			return  "1e4WfkBt1W9BOKS7r6cjtfxUi_OCsxU1CjHzSvPHPn_o";
+		default:
+			break;
+		}
 		return prop.getProperty("workbook");
 	}
 
